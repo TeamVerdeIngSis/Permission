d1a1e65e31ab94450a0dfba9492bed34adec60cd
Mvrancic <mvrancic@mail.austral.edu.ar>
2024-09-19 17:28:28 -0300

    first push

---
+++ src/main/kotlin/com/github/teamverdeingsis/permission/PermissionApplication.kt
@@ -0,0 +1,11 @@
+package com.github.teamverdeingsis.permission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PermissionApplication
+
+fun main(args: Array<String>) {
+	runApplication<PermissionApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/teamverdeingsis/permission/PermissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.teamverdeingsis.permission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PermissionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

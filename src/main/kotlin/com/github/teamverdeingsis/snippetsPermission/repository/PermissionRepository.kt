6cc379ee6f9f47949c04a2a487191cd7e3e92abf
JeroEngel <85593415+JeroEngel@users.noreply.github.com>
2024-10-26 11:07:51 -0300

    permissions simple functions

---
+++ src/main/kotlin/com/github/teamverdeingsis/snippetsPermission/repository/PermissionRepository.kt
@@ -0,0 +1,13 @@
+package com.github.teamverdeingsis.snippetsPermission.repository
+
+import com.github.teamverdeingsis.snippetsPermission.model.Permission
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface PermissionRepository : JpaRepository<Permission, UUID> {
+    fun findByUserId(userId: String): List<Permission>
+    fun findBySnippetId(snippetId: UUID): List<Permission>
+    fun findByUserIdAndSnippetId(userId: String, snippetId: UUID): Permission?
+}
